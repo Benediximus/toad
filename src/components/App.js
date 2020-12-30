c20d261f37196ebf0e4ef3ca033039c2370de7ad
Benediximus <WilliamAderemi@gmail.com>
2020-12-30 15:15:58 -0500

    Boilerplate code for App component

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <form>
+                <input type="checkbox" />
+                <p>One</p>
+
+                <input type="checkbox" />
+                <p>One</p>
+
+                <input type="checkbox" />
+                <p>One</p>
+            </form>
+        </div>
+        
+    )
+}
+
+export default App
