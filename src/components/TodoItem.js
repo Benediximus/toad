20f9b5b7f5d8eb05b68bb82cce874839dd142e20
Benediximus <WilliamAderemi@gmail.com>
2020-12-31 20:15:37 -0500

    Added TodoItem component file

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TodoItem() {
+    return (
+        <div className="todo-item">
+            
+            <input type="checkbox" />
+            <p>Sharks are not the nemesis of the internet</p>
+            
+        </div> 
+    )
+}
+
+export default TodoItem;
