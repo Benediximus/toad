e2a9237d91c56c9f7061f902c6fdaadb12182964
Benediximus <WilliamAderemi@gmail.com>
2021-01-01 16:09:47 -0500

    Added prop functionality to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import todosData from '../todosData';
 
 function App() {
+
+    const todoComponents = todosData.map(function(entry) {
+        return (
+            <TodoItem key={entry.id} item={entry}/>
+        )
+    })
     return (
         <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {todoComponents}
         </div>
     )
 }
